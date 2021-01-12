6265f768f330473fe128ed6200fd19b04512d516
Astrit Marleku <a.marleku@gmail.com>
2021-01-12 09:56:40 +0100

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,16 @@
   <div>
     <h1 class="container"> Test Vue </h1>
   </div>
-    
 </template>
 
 <script>
 export default {
+
   name: 'HelloWorld',
   props: {
     msg: String
   }
+  
 }
 </script>
 
